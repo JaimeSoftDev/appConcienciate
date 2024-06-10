d8c10768c5f69e9dfe30d5f8791eaddfac7df17e
JaimeSoftDev <jaimesoftdev@gmail.com>
2024-06-10 06:31:37 +0200

    TerminadoFinal

---
+++ resources/views/components/auth-card.blade.php
@@ -0,0 +1,9 @@
+<!-- resources/views/components/auth-card.blade.php -->
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+    <div>
+        <img src="https://www.concienciate.es/wp-content/uploads/2024/01/concienciate_300_84.png" alt="">
+    </div>
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        {{ $slot }}
+    </div>
+</div>
